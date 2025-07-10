c7ee7c87b8660a37c9e0e55f7aa28c35ea41f2af
Taras-Bilyk <bilyk.taras2007@gmail.com>
2025-07-10 17:18:40 +0100

    added: screenshots folder. removed: useless files from fonts. cnanged: file structure

--- bread/js/burger.js
+++
@@ -1,7 +0,0 @@
-var hamburger = document.querySelector(".burger")
-var menu = document.querySelector(".naw_menu")
-
-hamburger.onclick = function(){
-    hamburger.classList.toggle("burger_open")
-    menu.classList.toggle("menu_open")
-}
